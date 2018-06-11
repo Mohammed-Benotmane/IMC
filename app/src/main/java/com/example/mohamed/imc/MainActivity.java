e6e66e0f8f7d74a18ac6ae77023207dfb011eba1
Mohammed-Benotmane <mohammedbenotmanetom@gmail.com>
2018-06-11 16:19:02 +0200

    delete action bar

--- app/src/main/java/com/example/mohamed/imc/MainActivity.java
+++ app/src/main/java/com/example/mohamed/imc/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mohamed.imc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,15 +11,39 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
         final String a = "a";
         final String b = "b";
+        EditText taille;
+        EditText poids;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        taille = (EditText)findViewById(R.id.taille);
+        poids = (EditText)findViewById(R.id.poids);
+        super.onKeyDown(keyCode, event);
+        if (taille.getText().toString().equalsIgnoreCase("")){
+
+            return false;
+        }
+        else {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+            taille.setText( String.valueOf(Integer.parseInt(taille.getText().toString())-1));
+            return true;
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP){
+            taille.setText( String.valueOf(Integer.parseInt(taille.getText().toString())+1));
+            return true;
+        }}
+        return false;
+    }
+
     public void calculer(View view){
-        EditText taille = (EditText)findViewById(R.id.taille);
-        EditText poids = (EditText)findViewById(R.id.poids);
+        taille = (EditText)findViewById(R.id.taille);
+        poids = (EditText)findViewById(R.id.poids);
         if (taille.getText().toString().equals("")){
             Toast.makeText(MainActivity.this,"Veuillez introduire votre taille",Toast.LENGTH_SHORT).show();
             return;
